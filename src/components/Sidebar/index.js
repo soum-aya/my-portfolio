0f029fa48e64dc5b8ff175e941971fa17ec03719
Soumaya <soumaya.assadiki@gmail.com>
2022-06-30 19:30:50 +0100

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+    </div>
+  )
+}
+
+export default Sidebar
